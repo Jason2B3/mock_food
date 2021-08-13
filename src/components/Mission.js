806a51572725b88b0513cd90581a86ded34b5f75
jason <2B3>
2021-08-12 23:59:51 -0400

    can't remove a line break so I'm going to switch from desktop first to mobile first

--- src/components/Mission.js
+++ src/components/Mission.js
@@ -6,12 +6,12 @@ function Mission() {
     <section className={styles.container}>
       <h2>Delicious Food, Delivered to You</h2>
       <p>
-        Choose your favorite meal from our broad selection of available meals
-        and enjoy a delicious lunch or dinner at home.
+      <span id={styles.s1}>Choose your favorite meal from our broad selection of available meals
+        and enjoy a </span><span id={styles.s2}>delicious lunch or dinner at home.</span>
       </p>
       <p>
-        All our meals are cooked with high-quality ingredients, just-in-time and
-        of course by experienced chefs!
+      <span id={styles.s3}>All our meals are cooked with high-quality ingredients, just-in-time and
+        of course by </span><span id={styles.s4}>experienced chefs!</span>
       </p>
     </section>
   );
