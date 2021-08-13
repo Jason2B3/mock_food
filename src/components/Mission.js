3b36bbc8c0833fab6fd6ff7f2812b082f9598e98
jason <2B3>
2021-08-13 01:40:37 -0400

    learned how to remove line breaks

--- src/components/Mission.js
+++ src/components/Mission.js
@@ -6,12 +6,18 @@ function Mission() {
     <section className={styles.container}>
       <h2>Delicious Food, Delivered to You</h2>
       <p>
-      <span id={styles.s1}>Choose your favorite meal from our broad selection of available meals
-        and enjoy a </span><span id={styles.s2}>delicious lunch or dinner at home.</span>
+        <span id={styles.s1}>
+          Choose your favorite meal from our broad selection of available meals
+          and enjoy a{" "}
+        </span>
+        <span id={styles.s2}>delicious lunch or dinner at home.</span>
       </p>
       <p>
-      <span id={styles.s3}>All our meals are cooked with high-quality ingredients, just-in-time and
-        of course by </span><span id={styles.s4}>experienced chefs!</span>
+        <span id={styles.s3}>
+          All our meals are cooked with high-quality ingredients, just-in-time
+          and of course by{" "}
+        </span>
+        <span id={styles.s4}>experienced chefs!</span>
       </p>
     </section>
   );
